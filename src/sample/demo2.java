a8e4edcb8d5c9285103178bc455bc56f6ed0c52e
Shubham <Shubham@Shubham>
2021-08-03 11:47:43 +0530

    [shubham]:add to git hub

--- src/sample/demo2.java
+++ src/sample/demo2.java
@@ -1,5 +1,13 @@
 package sample;
 
 public class demo2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+	}
 
 }
